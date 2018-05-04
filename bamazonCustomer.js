83b801ee39af196779c2291a1015b95b6ec57fd4
Reinaldo Llano <llanoreinaldo@gmail.com>
2018-05-04 18:22:04 -0400

    commit js file changes

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,5 @@
 var mysql = require("mysql");
-var inquirer = require ("inquirer");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
 
@@ -11,7 +11,79 @@ var connection = mysql.createConnection({
 
     //My Password
     password: "Reinaldo33!",
-    database: "bamazon",
+    database: "bamazon"
 
-})
+});
 
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    readProducts();
+});
+
+
+function readProducts() {
+    var query = "SELECT * FROM products";
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        console.log("Bamazon Store Iventory List");
+        console.log("-----------------------------------------------------");
+        for (var i = 0; i < res.length; i++) {
+            console.log("Product: " + res[i].product_name + " | Avail Qty: " + res[i].stock_quantity);
+        };        
+        orderProducts();
+},
+)};
+
+function orderProducts() {
+    var query = "SELECT * FROM products";
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        inquirer
+            .prompt([{
+                    name: "choice",
+                    type: "list",
+                    choices: function () {
+                        var choiceArray = [];
+                        for (var i = 0; i < res.length; i++) {
+                            choiceArray.push(res[i].product_name)
+                        }
+                        return choiceArray;
+                    },
+                    message: "Which item would you like to buy?"
+                },
+                {
+                    name: "quantity",
+                    type: "input",
+                    message: "How many would you like to buy?"
+                },
+            ])
+            .then(function (answer) {
+                var chosenItem;
+                for (var i = 0; i < res.length; i++) {
+                    if (res[i].product_name === answer.choice) {
+                        chosenItem = res[i]
+                    }
+                }
+
+                if (chosenItem.stock_quantity > parseInt(answer.quantity)) {
+                    connection.query(
+                        "UPDATE products SET ? WHERE ?", 
+                    [
+                        {
+                            stock_quantity: stock_quantity - answer.quantity
+                        },
+                        {
+                            item_id: chosenItem.item_id
+                        }
+                    ],
+                    function(error) {
+                        if (error) throw err;
+                        console.log("Sorry, we don't have enough inventory")
+                    }
+                    )
+                }
+
+            })
+    })
+};
